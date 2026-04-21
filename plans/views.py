aa01143abe9cf18a4935b94d951147bc5fb344ee
HelniseS <helnise-santos12@hotmail.com>
2026-04-21 23:45:54 +0100

    add workout plans to the homepage

--- plans/views.py
+++ plans/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import WorkoutPlan
 
 # Create your views here.
 def home(request):
-    return render(request, 'plans/home.html')
+    plans = WorkoutPlan.objects.all()[:3]
+    return render(request, 'plans/home.html', {'plans': plans})
