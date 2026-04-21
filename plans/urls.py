dd78ca3a9e387d923cabc7c58b05f5ea13374f0c
HelniseS <helnise-santos12@hotmail.com>
2026-04-22 00:12:55 +0100

    add plan list and plan detail pages

--- plans/urls.py
+++ plans/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path 
-from  . import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home') ,
+    path('', views.home, name='home'),
+    path('plans/', views.plan_list, name='plan_list'),
+    path('plans/<slug:slug>/', views.plan_detail, name='plan_detail'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import WorkoutPlan
 
-# Create your views here.
+
 def home(request):
     plans = WorkoutPlan.objects.all()[:3]
     return render(request, 'plans/home.html', {'plans': plans})
+
+
+def plan_list(request):
+    plans = WorkoutPlan.objects.all()
+    return render(request, 'plans/plan_list.html', {'plans': plans})
+
+
+def plan_detail(request, slug):
+    plan = get_object_or_404(WorkoutPlan, slug=slug)
+    return render(request, 'plans/plan_detail.html', {'plan': plan})
