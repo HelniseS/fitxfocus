e25160e5801b3be636fcb9280496427ed9e8617a
HelniseS <helnise-santos12@hotmail.com>
2026-04-24 23:44:35 +0100

    use database url for heroku

--- fitfocus/settings.py
+++ fitfocus/settings.py
@@ -69,10 +69,11 @@ WSGI_APPLICATION = 'fitfocus.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 
