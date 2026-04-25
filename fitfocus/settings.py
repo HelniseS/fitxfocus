052be42bc2180890cdfc7c2503a3173a6ca91e90
HelniseS <helnise-santos12@hotmail.com>
2026-04-25 02:19:20 +0100

    fix static root

--- fitfocus/settings.py
+++ fitfocus/settings.py
@@ -110,8 +110,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
