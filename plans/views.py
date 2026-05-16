009be4a04e8ce189d6654c2383913128629cbea3
HelniseS <helnise-santos12@hotmail.com>
2026-05-16 16:36:37 +0100

    add user facing CRUD functionality

--- plans/views.py
+++ plans/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import WorkoutPlan
 from checkout.models import Purchase
+from .forms import WorkoutPlanForm
 
 
 def home(request):
@@ -29,4 +32,43 @@ def plan_detail(request, slug):
         "plan": plan,
         "has_purchased": has_purchased,
     })
-    
+
+@login_required
+def create_plan(request):
+    if request.method == "POST":
+        form = WorkoutPlanForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect("plan_list")
+    else:
+        form = WorkoutPlanForm()
+
+    return render(request, "plans/create_plan.html", {"form": form})
+
+
+@login_required
+def edit_plan(request, slug):
+    plan = get_object_or_404(WorkoutPlan, slug=slug)
+
+    if request.method == "POST":
+        form = WorkoutPlanForm(request.POST, request.FILES, instance=plan)
+
+        if form.is_valid():
+            form.save()
+            return redirect("plan_detail", slug=plan.slug)
+    else:
+        form = WorkoutPlanForm(instance=plan)
+
+    return render(request, "plans/edit_plan.html", {"form": form, "plan": plan})
+
+
+@login_required
+def delete_plan(request, slug):
+    plan = get_object_or_404(WorkoutPlan, slug=slug)
+
+    if request.method == "POST":
+        plan.delete()
+        return redirect("plan_list")
+
+    return render(request, "plans/delete_plan.html", {"plan": plan})
