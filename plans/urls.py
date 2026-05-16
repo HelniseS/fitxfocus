63203bb5136a41e23138bccda130a73435212dac
HelniseS <helnise-santos12@hotmail.com>
2026-05-16 16:50:46 +0100

    add urls for workout plan CRUD views

--- plans/urls.py
+++ plans/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('plans/', views.plan_list, name='plan_list'),
-    path('plans/<slug:slug>/', views.plan_detail, name='plan_detail'),  
+    path("create/", views.create_plan, name="create_plan"),
+    path('plans/<slug:slug>/', views.plan_detail, name='plan_detail'),
+    path("<slug:slug>/edit/", views.edit_plan, name="edit_plan"),
+    path("<slug:slug>/delete/", views.delete_plan, name="delete_plan"),  
 ] 
